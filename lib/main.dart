4c29df0fa9392aa093efc5dd660ae430ef8be5ae
Aizaz Haider <aizazhaider720@gmail.com>
2022-09-01 16:36:00 +0500

    API Part 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
-import 'package:flutter/cupertino.dart';
+import 'dart:convert';
+import 'dart:io';
+import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
-
+import 'dart:convert' as convert;
 import 'dart:developer' as devtools show log;
 
 extension Log on Object {
@@ -11,50 +13,11 @@ void main() {
   runApp(const MyApp());
 }
 
-// We use Future for Asynchronous Tasks
-testIt1() async {
-  print("Step 1");
-  Future(
-    () {
-      print("Step 2");
-    },
-  );
-
-  // then is try block & .catchError is catch block
-  // If Future executes successfully after that then will get value
-  Future.delayed(
-    const Duration(seconds: 2),
-    () => "Wait Completed Successfully After 2 seconds",
-  ).then((value) => print(value));
-
-  // Then will not execute due to exception now catching exception
-  Future.delayed(
-    const Duration(seconds: 2),
-    () => throw "Wait Completed Exception",
-  ).then((value) => print(value)).catchError((e) => print(e));
-
-  // If Future executes successfully after that then will get value
-  await Future.delayed(
-    const Duration(seconds: 5),
-    () => "Wait Completed Successfully 5 seconds",
-  ).then((value) => print(value));
-
-  print("Step-3");
-  print("Database Opened");
-}
-
-testIt2() async {
-  print("Before Opening DB");
-  await testIt1();
-  print("DB Opened Successfully");
-}
-
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    testIt2();
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
@@ -62,90 +25,85 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: HomePage(),
-      onGenerateRoute: (settings) {
-        final args = settings.arguments;
-        if (settings.name == "about/") {
-          if (args is Map<String, String>) {
-            return MaterialPageRoute(
-              builder: (context) => AboutPage(map: args),
-            );
-          }
-        }
-      },
     );
   }
 }
 
 class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
   @override
   State<HomePage> createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
-  String? gettingBack;
+  String? nameFromApi = "";
+  // Example 1 with http Package
+  callApi() async {
+    try {
+      setState(() {
+        nameFromApi = null;
+      });
+      var url = Uri.https("api.github.com", "/users/aizazisonline");
+      var response = await http.get(url);
+      var jsonResponse =
+          convert.jsonDecode(response.body) as Map<String, dynamic>;
+      jsonResponse["name"].toString().log();
+      setState(() {
+        nameFromApi = jsonResponse["name"];
+      });
+    } catch (e) {
+      e.log();
+    }
+  }
+
+  // Example 2 without http Package
+  parseJson() async {
+    try {
+      setState(() {
+        nameFromApi = null;
+      });
+      const String url = "https://api.github.com/users/ishaquehassan";
+      final result = await HttpClient()
+          .getUrl(Uri.parse(url))
+          .then((request) => request.close())
+          .then((response) => response.transform(utf8.decoder).join())
+          .then(
+              (jsonString) => json.decode(jsonString) as Map<String, dynamic>);
+      // .then((json) => json.map((map) => Person.fromJson(map)));
+      result["name"].toString().log();
+      setState(() {
+        nameFromApi = result["name"];
+      });
+    } catch (e) {
+      e.log();
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Home Page'),
+        title: const Text('API Sample'),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Text("Data of Home Page", style: TextStyle(fontSize: 25)),
-            Text(
-              gettingBack ?? "",
-              style: const TextStyle(fontSize: 25),
+            if (nameFromApi != null)
+              Text(
+                nameFromApi!,
+                style: const TextStyle(fontSize: 30),
+              ),
+            if (nameFromApi == null) const CircularProgressIndicator(),
+            ElevatedButton(
+              onPressed: callApi,
+              child: const Text("Call API 1"),
             ),
             ElevatedButton(
-                onPressed: () {
-                  Navigator.pushNamed(context, "about/", arguments: {
-                    "title": "this is title",
-                    "desc": "this is desc",
-                  }).then((value) {
-                    setState(() {
-                      gettingBack = value as String;
-                    });
-                  });
-                },
-                child: const Text("Go to About Page",
-                    style: TextStyle(fontSize: 25))),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class AboutPage extends StatelessWidget {
-  final Map map;
-
-  const AboutPage({Key? key, required this.map}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.red,
-        title: const Text('About Page', style: TextStyle(fontSize: 25)),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text("Data of About Page", style: TextStyle(fontSize: 25)),
-            Text(map["title"].toString(), style: const TextStyle(fontSize: 25)),
-            Text(map["desc"].toString()),
-            ElevatedButton(
-                style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all(Colors.red),
-                ),
-                onPressed: () {
-                  Navigator.pop<String>(context, "Sending Data Back");
-                },
-                child: const Text("Go to Home Page")),
+              onPressed: parseJson,
+              child: const Text("Call API 2"),
+            ),
           ],
         ),
       ),
